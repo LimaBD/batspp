dd5689feb5f7a06242b293cd8027342a3119720d
LimaBD <bdl1998@hotmail.com>
2022-08-02 19:14:18 -0300

    settings file first commit

---
+++ batspp/_settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#
+# Settings
+#
+# This contains constants required
+# by multiple local modules
+#
+
+
+"""Settings module"""
+
+
+BATSPP_EXTENSION = 'batspp'
+
+BATS_EXTENSION = 'bats'
