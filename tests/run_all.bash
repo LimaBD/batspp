3c44578fd36a8270d9d34571643120d7f57311b4
LimaBD <bdl1998@hotmail.com>
2022-08-09 16:15:48 -0300

    added base path to PYTHONPATH

--- tests/run_all.bash
+++ tests/run_all.bash
@@ -5,6 +5,7 @@
 ## TODO: save older runned htmlcovs.
 
 tests=$(dirname $(realpath -s $0))
+export PYTHONPATH="$tests/../:$PYTHONPATH"
 echo -e "Running tests on $tests\n"
 coverage run -m unittest discover $tests
 coverage html --directory $tests/htmlcov
