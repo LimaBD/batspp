7cf64560a0e061426d75ec0c0b478bfcde2364c0
LimaBD <bdl1998@hotmail.com>
2022-08-15 20:39:18 -0300

    omitted tests files on coverage report

--- tools/run_tests.bash
+++ tools/run_tests.bash
@@ -10,4 +10,4 @@ tests=$base/tests
 echo -e "Running tests on $tests\n"
 export PYTHONPATH="$base/:$PYTHONPATH"
 coverage run -m unittest discover $tests
-coverage html --directory $tests/htmlcov
+coverage html --directory $tests/htmlcov --omit="*/tests/test_*"
