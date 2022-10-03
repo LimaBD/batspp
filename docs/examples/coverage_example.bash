1a82dd6125e8c4fac82c091b90b3708e5b5cb100
LimaBD <bdl1998@hotmail.com>
2022-10-03 13:35:38 -0300

    fix test file not founded due to path

--- docs/examples/coverage_example.bash
+++ docs/examples/coverage_example.bash
@@ -6,4 +6,6 @@
 # We need to exclude Bats related files
 #
 
-kcov --exclude-pattern=/usr/lib/,/tmp/bats-run ./report/ bats ./generated_bash_example.bats 
+this_folder=$(dirname $(realpath -s $0))
+cd $this_folder
+kcov --exclude-pattern=/usr/lib/,/tmp/bats-run $this_folder/report/ bats ./generated_bash_example.bats 
